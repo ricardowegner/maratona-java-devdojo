80ac4fc2505ec156afb2efc92d897714ad19cf63
Ricardo Wegner <ricardowegneroliveira@gmail.com>
2023-09-12 10:22:12 -0400

    Mesmo exercicio só que usando continue

--- src/academy/maratona/java/introducao/Aula06EstruturasDeRepeticao05.java
+++ src/academy/maratona/java/introducao/Aula06EstruturasDeRepeticao05.java
@@ -1,5 +1,18 @@
 package academy.maratona.java.introducao;
 
 public class Aula06EstruturasDeRepeticao05 {
-  
+    // Dado um valor de um carro, descubra quantas vezes ele pode ser parcelado
+    // A condição é que a valor da parcela tem que ser maior ou igual a 1000
+    public static void main(String[] args) {
+        double valorTotal = 30000;
+        for (int parcela = (int) valorTotal; parcela >= 1; parcela--) {
+            double valorParcela = valorTotal / parcela;
+            if(valorParcela < 1000) {
+                continue;
+            }
+            System.out.println("Número de parcelas: " + parcela + " Valor parcela: " + valorParcela);
+
+        }
+
+    }
 }
